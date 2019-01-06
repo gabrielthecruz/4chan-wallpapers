4906397361b147021c4afa02471ca2ad755e6516
Gabriel Cruz <gabriel.cruz232@gmail.com>
2019-01-06 14:04:40 -0200

    Update 4chan-wallpapers.py
    Finished code and improved some details.

--- 4chan-wallpapers.py
+++ 4chan-wallpapers.py
@@ -1,29 +1,46 @@
+from sys import argv as args
 import requests
 import json
-from sys import argv as args
+import os
 
-board, width, height, limit, folder = args[1:] or ['wg', '1920', '1080', '-1', './Wallpapers']
+"""
+Checks the resolution of a post image.
+"""
+def check_post(post, resolution, limit):
+	if 'filename' not in post.keys() or limit not in ['max', 'min', 'equal']  and post['ext'] != 'jpg':
+		return False
 
-limit = int(limit)
-resolution = (int(width), int(height))
-folder = (folder if folder.endswith('/') else folder + '/') + '%s'
+	results = {
+		'equal': (post['w'], post['h']) == resolution,
+		'max'  : post['w'] <= resolution[0] and post['h'] <= resolution[1],
+		'min'  : post['w'] >= resolution[0] and post['h'] >= resolution[1]
+	}
 
-t_api_url  = 'https://a.4cdn.org/%s/' % board
-i_api_url  = 'https://i.4cdn.org/%s/' % board
-resolution = (str(width), str(height))
+	return results[limit]
 
-pages = json.loads(requests.get(t_api_url + 'threads.json').text)
-threads = [val['no'] for sublist in pages for val in sublist['threads']]
+configs = {
+	'resolution': tuple(map(int, args[2].split('x'))),
+	'limit'     : args[3],
+	'folder'    : (args[4] if args[4].endswith('/') else args[4] + '/') + '%s',
+	'api_urls'  : 'https://a.4cdn.org/%s/' % args[1]
+}
+
+if not os.path.exists(configs['folder'][:-2]):
+	os.makedirs(configs['folder'][:-2])
+
+pages   = json.loads(requests.get(configs['api_urls'] + 'threads.json').text)
+threads = [thread['no'] for threads in pages for thread in threads['threads']]
+images_saved = 0
 
 for thread in threads:
-	print('Searching in thread no.%d' % thread)
-	posts = json.loads(requests.get(t_api_url + 'thread/%d.json' % thread).text)['posts']
+	posts = json.loads(requests.get(configs['api_urls'] + 'thread/%d.json' % thread).text)['posts']
 
-	for post in filter(lambda post: 'filename' in post.keys() and (post['w'], post['h']) == resolution, posts):
+	for post in filter(lambda post: check_post(post, configs['resolution'], configs['limit']), posts):
 		filename = str(post['tim']) + post['ext']
 
-		image_request = requests.get(i_api_url + filename, 'wb')
-		with open(folder % filename, 'wb') as image_file:
-			image_file.write(image_request.content)
+		with open(configs['folder'] % filename, 'wb') as file:
+			file.write(requests.get('https://i.4cdn.org/%s/%s' % (args[1], filename)).content)
 
-		print('\tSaved ' + filename)
+		images_saved += 1
+
+print('Found %d wallpapers on /%s/!' % (images_saved, args[1]))
