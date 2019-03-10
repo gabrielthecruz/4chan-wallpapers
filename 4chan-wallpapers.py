20f9330b4ba6305577c9bb71472f266c5f6eef7e
Gabriel Cruz <gabriel.cruz232@gmail.com>
2019-03-10 15:11:33 -0300

    Código refeito
    Todo o código foi refeito usando dois novos módulos: argparse, para tratar argumentos da linha de comando; e logging para imprimir ou não as mensagens na tela.
    Outros pequenos ajustes foram feitos na rotina em geral pra simplificar o código.

--- 4chan-wallpapers.py
+++ 4chan-wallpapers.py
@@ -1,34 +1,128 @@
 from urllib import request
-from sys import argv
-import os
+import argparse
+import logging
 import json
+import os
 
-def check_image(resolution, operator):
-	"""Creates a function to validate the image resolution."""
-	return eval('lambda r: r %s resolution' % operator)
 
-board, resolution, operator, directory = argv[1:] + [''] * 4
-image_filter = check_image(tuple(map(int, resolution.split('x'))), operator)
+def get_response(url, logger):
+    """
+    Requisita um GET e retorna a resposta obtida em caso de sucesso.
 
-if not os.path.exists(directory):
-	os.makedirs(directory)
+    Feito o request, sua resposta é validada e retornada.
+    Em caso de erro no request ou na validação, retorna None.
+    Todas as mensagens são tratadas pelo logger.
+    """
+    try:
+        logger.info('GET Request: {}'.format(url))
+        response = request.urlopen(url)
+    except Exception as error:
+        logger.error(error)
+        response = None
+    else:
+        code = response.getcode()
+        if code != 200:
+            logger.error('Resposta inválida do servidor. Código: ' + str(code))
+            headers = ['\t{}: {}'.format(*h) for h in response.getheaders()]
+            map(logger.info, headers)
+            response = None
+    finally:
+        return response
 
-req = request.urlopen('https://a.4cdn.org/%s/threads.json' % board)
-pages = json.loads(req.read().decode())
-threads = [thread['no'] for threads in pages for thread in threads['threads']]
-images_saved = 0
 
-for thread in threads:
-	req = request.urlopen('https://a.4cdn.org/{}/thread/{}.json'.format(board, thread))
-	posts = json.loads(req.read().decode())
-	filenames = [str(post['tim']) + post['ext'] for post in posts['posts'] if 'w' in post and image_filter((post['w'], post['h']))]
+def get_filename(post, filter_type, resolution):
+    """
+    Valida um post e retorna o nome do arquivo ou uma string vazia.
 
-	for filename in filenames:
-		req = request.urlopen('https://i.4cdn.org/{}/{}'.format(board, filename))
+    Recebe um dicionário referente a um post, verifica se há uma imagem,
+    a filtra e, se válida, retorna o nome do arquivo.
+    """
+    post_res = [post.get('w'), post.get('h')]
+    post_ext = post.get('ext')
+    filename = '{}{}'.format(post.get('tim'), post_ext)
 
-		with open(os.path.join(directory, filename), 'wb') as file:
-			file.write(req.read())
+    if (None in post_res) or (post_ext not in ['.jpg', '.jpeg', '.png']) or (
+        filter_type == 'EQUAL' and post_res != resolution) or (
+            filter_type == 'MAX' and post_res > resolution) or (
+            filter_type == 'MIN'and post_res < resolution):
+        filename = ''
 
-	images_saved += len(filenames)
+    return filename
 
-print('Found {} wallpapers on /{}/!'.format(images_saved, board))
+
+# Configuração dos argumentos da linha de comando
+parser = argparse.ArgumentParser(
+    description='Busca e baixa wallpapers de uma board do 4chan.')
+parser.add_argument(
+    '-r',
+    '--resolution',
+    type=int,
+    nargs=2,
+    metavar=('WIDTH', 'HEIGHT'),
+    required=True,
+    help='Resolução desejada dos wallpapers')
+parser.add_argument(
+    '-b', '--board', default='wg', help='Id da board sem as barras')
+parser.add_argument(
+    '-f',
+    '--filter',
+    choices=['MIN', 'MAX', 'EQUAL'],
+    default='EQUAL',
+    help='Filtro aplicado às imagens encontradas na board')
+parser.add_argument(
+    '-d',
+    '--destination',
+    default=os.path.join(os.getcwd(), 'Wallpapers'),
+    help='Onde serão salvos os wallpapers')
+parser.add_argument(
+    '-v',
+    '--verbose',
+    action='store_true',
+    help='Imprime informações e erros na tela')
+args = parser.parse_args()
+
+# Configurar exibição de mensagens na tela
+logging.basicConfig(
+    level=logging.NOTSET,
+    format='%(message)s',
+    handlers=[logging.NullHandler()])
+logger = logging.getLogger()
+if args.verbose:
+    logger.addHandler(logging.StreamHandler())
+
+if not os.path.exists(args.destination):
+    os.makedirs(args.destination)
+
+api_url = 'https://{}.4cdn.org/{}/{}'
+
+res = get_response(api_url.format('a', args.board, 'threads.json'), logger)
+pages = [] if res is None else json.load(res)
+threads = [thrd['no'] for thrds in pages for thrd in thrds['threads']]
+downloads = 0
+
+for thread_id in threads:
+    res = get_response(
+        api_url.format('a', args.board, 'thread/{}.json'.format(thread_id)),
+        logger)
+
+    if res is None:
+        continue
+
+    posts = json.load(res)
+    filenames = map(lambda p: get_filename(p, args.filter, args.resolution),
+                    posts['posts'])
+
+    for filename in filter(bool, filenames):
+        res = get_response(api_url.format('i', args.board, filename), logger)
+
+        if res is None:
+            continue
+
+        image = open(os.path.join(args.destination, filename), 'wb')
+        image.write(res.read())
+        image.close()
+
+        logger.info('Arquivo {!r} baixado'.format(filename))
+        downloads += 1
+
+logger.info('Arquivos baixados: {}'.format(downloads))
